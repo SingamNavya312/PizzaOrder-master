1e8524249e5bda4a7488e0154556144291bd98b5
SingamNavya312 <86612637+SingamNavya312@users.noreply.github.com>
2022-04-28 10:07:08 +0530

    PizzaOrder

--- PizzaOrder-master/src/main/java/com/abc/pizza/service/PizzaAccountService.java
+++ PizzaOrder-master/src/main/java/com/abc/pizza/service/PizzaAccountService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.abc.pizza.entity.PizzaAccount;
-import com.abc.pizza.entity.PizzaAccount;
+
 import com.abc.pizza.exceptions.InvalidUserException;
 
 @Service
@@ -19,5 +19,9 @@ public interface PizzaAccountService
 	public List<PizzaAccount> getAllPizza();
 	public boolean insertPizzaAccount(PizzaAccount a);
 	public PizzaAccount getPizzaAccount(int pId);
+
+	
+	
+	public Object updatePizzaAccount(Long pizzaId);
 	
 }
--- PizzaOrder-master/src/main/java/com/abc/pizza/web/PizzaAccountRestController.java
+++ PizzaOrder-master/src/main/java/com/abc/pizza/web/PizzaAccountRestController.java
@@ -1,11 +1,16 @@
 package com.abc.pizza.web;
 
+
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,4 +38,21 @@ public class PizzaAccountRestController {
 	
 		return status == true?"Pizza Added ":"Contact to customer Care";
 	}
+	
+	//update operation
+	@PutMapping("/admin/pizza/{id}")
+	public PizzaAccount updatePizzaAccount(@RequestBody PizzaAccount pizzaAccount, @PathVariable("id") Long pizzaId)
+	{
+		return PizzaAccountService.updatePizzaAccount(pizzaId);
+	}
+	
+	//Delete operation
+	@DeleteMapping("/admin/pizza/{id}")
+	
+	public String deletePizzaAccountById(@PathVariable("id") Long pizzaId)
+	{
+		PizzaAccountService.deletePizzaAccountById(pizzaId);
+		return "Deleted Successfully";
+	}
+	
 }
